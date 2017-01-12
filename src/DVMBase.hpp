ede02e96defe3d82a7d74ad2838347c57fe557a7
Mark Bruggemann <mark.bruggemann@gmail.com>
2017-01-12 20:39:06 +0000

    Documentation of the DVMBase class
    No functional changes to code

--- src/DVMBase.hpp
+++ src/DVMBase.hpp
@@ -14,48 +14,146 @@
 #include <string.h>
 #include <string>
 
+/// DMV class contaning all the elements of the DVM
+
 class DVMBase
 {
+	private:
+	VortexBlobs m_vortex;    ///< The point vortices
+	VortexSheet m_vortsheet; ///< The vortex sheet
+	Body m_body;             ///< The solid body
+	VortexBlobs m_probe;     ///< The probe???
+
+	Matrix m_infM; ///< Influence matrix
+
+	/** @name Output files */
+	///@{
+	std::ofstream dev_dvm;   ///< Vortex position
+	std::ofstream dev_Num;   ///< Number of vortices
+	std::ofstream dev_gamma; ///< Vortex sheet strength
+	std::ofstream dev_loads; ///< Forces
+	std::ofstream dev_probe; ///< Velocity
+	///@}
+
+	double m_dt; ///< Timestep
+	double m_nu; ///< Kinematic viscosity
+	double m_Ux; ///< Free stream velocity in x
+	double m_Uz; ///< Free stream velocity in ym_n, m_np;
+
+	double m_n; ///< Number of body points
+
+	double m_kernel_threshold; ///< Kernel threshold
+	double m_sigma_cutoff;     ///< Cut-off distance
+
+	double m_time;       ///< Current simulation time
+	unsigned m_step;     ///< Current timestep
+	unsigned m_steps;    ///< Total timesteps
+	unsigned m_maxGamma; ///< Maximum value of the vorticity
+
+	/** @name Reading and writing */
+
+	///@{
+	std::string m_in_dir;      ///< input directory
+	std::string m_out_dir;     ///< output directory
+	std::string m_domain_file; ///< body geometry file
+	///@}
+
+	// some constants
+	double m_pi;   ///< pi
+	double m_rpi2; ///< 1 / 2pi
+
+	// Loads related variables
+	double m_fx;  ///< Force on the body in x
+	double m_fz;  ///< Force on the body in z
+	double m_rho; ///< Density of the fluid
+
+	private:
 	public:
 	DVMBase();
 
-	// initialise the object
+	/** @name General Initialisation */
+	///@{
+
+	/// Initialise the object
 	void init(pugi::xml_document &);
 
-	// read paramters
-	void init_dipole(double zdistance,
-	                 double xdistance,
-	                 unsigned nvb1,
-	                 unsigned nvb2,
-	                 double radius1,
-	                 double radius2);
-	void init_outputs();
+	/// Read the input geometry file
 	void read_input_coord();
 
-	// form matrices / and computational entinties
-	void form_vortex_sheet();
+	/// Initialise the output files
+	void init_outputs();
+	///@}
+
+	/// Compute the influence matrix
+	/** Computed using the coefficients after Morgenthal */
 	void compute_influence_matrix();
 
-	// Convect stuff
-	void biotsavart();
+	/** @name Vortex sheet related methods */
+	///@{
+
+	/// Initialise the vortex sheet
+	void form_vortex_sheet();
+
+	/// Solve for the vortex sheet
+	void solvevortexsheet();
+
+	/// Update the vorticity on each sheet
+	void save_vort();
+	///@}
+
+	/// Compute the forces on the body
+	void compute_loads();
+
+	/// Convect point vortices
 	void convect(unsigned order);
 
-	// Vortex sheet stuff
-	void solvevortexsheet();
-	void vortexsheetbc();
-	void imagebc();
+	/** @name Methods related to diffusion */
+	///@{
 
-	// Diffusion stuff
+	/// Move the point vortices using the random walk
 	void diffrw();
-	void diffpse(double nu, double dt);
+
+	/// Generate vorticity on the boundary and release into the flow
 	void diffuse_vs_rw();
+	///@}
+
+	/// Reflect particles generated released inside the body outside
+	void reflect();
+
+	/// Write the output file
+	/** Happens at each time step */
 	void write_outputs();
 
-	// Housekeeping stuff
-	void delete_vort();
-	void reflect();
-	void absorb();
-	int inside_body(double x, double z);
+	/** @name Timeloop control */
+	///@{
+
+	/// Increment the time step
+	void increment_step();
+
+	/// Get the size of the vortex sheet
+	unsigned get_vs_size();
+
+	/// Get number of total timesteps
+	unsigned get_steps();
+
+	/// Simulation time of the current time step
+	double get_time();
+	///@}
+
+	/// Total number of point vortices at a given time
+	unsigned get_size();
+
+	/// Get the velocities at a given point
+	void probe_velocities();
+
+	private:
+	/// Compute the vortex sheet boundary condition
+	void vortexsheetbc();
+
+	/// Biot-Savart relationship
+	void biotsavart();
+
+	/// Mirror a particle from one side of the boundary to the other
 	std::vector<double> mirror(double x_init,
 	                           double z_init,
 	                           double x_0,
@@ -63,47 +161,40 @@ class DVMBase
 	                           double x_1,
 	                           double z_1);
 
+	/// Determine if a particle lies inside the solid body
+	int inside_body(double x, double z);
+
+	/// This is all the useless stuff below //////////////////////
+
+	/*
+	// read paramters
+	void init_dipole(double zdistance,
+	                 double xdistance,
+	                 unsigned nvb1,
+	                 unsigned nvb2,
+	                 double radius1,
+	                 double radius2);
+
+	// Vortex sheet stuff
+	void imagebc();
+
+	// Diffusion stuff
+	void diffpse(double nu, double dt);
+
+	// Housekeeping stuff
+	void delete_vort();
+	void absorb();
+
 	// Diagnostics
-	void save_vort();
-	void compute_loads();
 	void compute_strouhal();
-	void probe_velocities();
-	double get_time();
-	unsigned get_size();
-	unsigned get_vs_size();
-	unsigned get_steps();
 
-	void increment_step();
-
-	public:
-	VortexBlobs m_vortex;
-	VortexSheet m_vortsheet;
-	Body m_body;
-	VortexBlobs m_probe;
-
-	public:
-	Matrix m_infM;
-	std::ofstream dev_dvm, dev_Num, dev_gamma, dev_loads, dev_probe;
-	// std::vector<double> xi_rw, eta_rw; // Why were these here???
 	std::vector<double> uvs, wvs, uI, wI;
+	// std::vector<double> xi_rw, eta_rw; // Why were these here???
 	// std::vector<double> m_Gamma_abs;
-	double m_dt, m_nu, m_Ux, m_Uz, m_n, m_np;
-	double m_kernel_threshold, m_sigma_cutoff;
-	double m_time;
-	unsigned m_step, m_steps;
-	double m_GammaDel, m_maxGamma;
-	double m_St;
+	// double m_GammaDel,
+	// double m_St;
+	*/
 
-	// Reading and writing
-	std::string m_in_dir;
-	std::string m_out_dir;
-	std::string m_domain_file;
-
-	// some constants
-	double m_pi, m_rpi2;
-
-	// Loads related variables
-	double m_fx, m_fz, m_rho;
 };
 
 #endif
