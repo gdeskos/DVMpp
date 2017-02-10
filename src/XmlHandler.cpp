97fd824a676391b1c98b7389cc066bc60c61affe
Mark Bruggemann <mark.bruggemann@gmail.com>
2017-02-10 12:37:47 +0000

    Remove unused option from the handler map

--- src/XmlHandler.cpp
+++ src/XmlHandler.cpp
@@ -177,7 +177,6 @@ void XmlHandler::buildOptionMap()
 	
     m_option_map.insert({"constants_density", {"constants", "density", "val", "specific gravity of fluid", ">0"}});
 	m_option_map.insert({"constants_nu", {"constants", "nu", "val", "kinematic viscosity [m^2 / s]", ">0"}});
-	m_option_map.insert({"constants_max_Gamma", {"constants", "max_Gamma", "val", "maximum circulation of nascent vortices", ">0"}});
 	m_option_map.insert({"constants_max_NumPanelVort", {"constants", "max_NumPanelVort", "val", "maximum number of panel vortices", ">0"}});
 	m_option_map.insert({"constants_cutoff_exp", {"constants", "cutoff_exp", "val", "cutoff exp", "none"}});
 	m_option_map.insert({"constants_seed", {"constants", "seed", "val", "seed for rng. <0 random, >=0 seed", "none"}});
